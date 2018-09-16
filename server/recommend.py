8b2632d8fca14523fb47bab4840f2b3b9aa306cf
Cjen1 <cjj39@cam.ac.uk>
2018-09-16 09:34:49 +0100

    Implement recommendation engine

--- server/recommend.py
+++ server/recommend.py
@@ -1,31 +1,46 @@
 import numpy as np
 import numpy.random as rand
+import pickle
+import json
 
-class podcast:
-    def __init__(self, url, vec):
-        self.url = url
+class Podcast:
+    def __init__(self, title, vec):
+        self.title = title
         self.vec = np.array(vec)
 
     def cosine(self, v):
         return (np.transpose(self.vec) * v)/(np.linalg.norm(self.vec) * np.linalg.norm(v))
 
-    def get_recs(self, podcasts, n = 1):
-        angles = []
-        for podcast in podcasts:
-            angle = self.cosine(podcast.vec)
-            if angle != 0:
-                angles.append(podcast, angle)
-        angles.sort(key=lambda a: a[angle])
-        return angles
+#def gen_podcasts(n):
+#    podcasts = [Podcast(str(i), v) for i, v in enumerate([np.array([i, 1]) for i in range(n)])]
+#    return podcasts
 
-def gen_podcasts(n):
-    podcasts = [podcast(str(i), v) for i, v in enumerate([np.array([i, 1]) for i in range(n)])]
-    return podcasts
+def add_like(user_id, title):
+    liked = pickle.load("likes.pkl")
+    if not user_id in liked:
+        liked[user_id] = set()
 
-def get_rec(user_id):
-    liked = []
+    liked[user_id].add(title)
+
+    pickle.dump(liked, "likes.pkl")
+
+    return json.dump(liked)
+
+def get_rec(user_id, n = 1):
+    liked = pickle.load("likes.pkl")
+
+    df = pandas.read_csv("trained_embeddings.csv")
+    podcasts = {row['title']:Podcast(row['title'], row['vec']) for i, row in df.iterrows()}
+    
+    distances = {podcast.title:0 for podcast in podcasts}
     for like in liked:
+        temp_distances = {podcast.title: podcasts[like].cosine(podcast) for podcast in podcasts}
         
-    return user_id
+        for title, distance in temp_distances:
+            distances[title] *= distance
 
-        
+    distances = [(k, v) for k,v in distances]
+
+    distances.sort(key=lambda kv: kv[1])
+
+    return json.dumps(distance[:n])
--- server/server.py
+++ server/server.py
@@ -14,6 +14,10 @@ import recommend
 def get_recommendation():
     return recommend.get_rec(request.query['user_id'])
 
+@route('/like')
+def add_like():
+    return recommend.add_like(request.query['user_id','title'])
+
 run(host='localhost', port=8080)
     
 
